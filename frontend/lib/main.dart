20dc198b998c3cb454ee57d09703e5d7aea46e01
JAPH254 <omanyujapheth@gmail.com>
2024-06-14 20:35:24 +0300

    initial commit for the heaviety app

---
+++ frontend/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/LoginPage.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Heaviety Business Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Loginpage(),
+    );
+  }
+}
