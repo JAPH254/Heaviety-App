308e4ffa8d339e4598cb7e70b389070364b18cc2
JAPH254 <omanyujapheth@gmail.com>
2024-06-15 09:39:30 +0300

    Added the icons and the text input bar

--- frontend/lib/ChatPage.dart
+++ frontend/lib/ChatPage.dart
@@ -6,37 +6,51 @@ class Chatpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 6, 48, 82),
-          title: Text('Heaviety Business Chat',
-              style: TextStyle(color: Colors.blue.shade300)),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  print('logging out');
-                },
-                icon: Icon(Icons.logout)),
-          ],
-        ),
-        body: ListView(
-          children: [
-            Container(
-              child: Text(
-                'Welcome to Heaviety Business Chat',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue.shade300,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 6, 48, 82),
+        title: Text('Heaviety Business Chat',
+            style: TextStyle(color: Colors.blue.shade300)),
+        actions: [
+          IconButton(
+              onPressed: () {
+                print('logging out');
+              },
+              icon: Icon(Icons.logout)),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter your message',
+                        border: OutlineInputBorder()),
+                  ),
                 ),
-              ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.send))
+              ],
             ),
-            Container(
-              child: Image.network(
-                'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600',
-                fit: BoxFit.cover,
-              ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.add, color: Colors.blue.shade300)),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.send, color: Colors.blue.shade300)),
+              ],
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
